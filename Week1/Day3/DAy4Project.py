1007234c348ca1774097a3f3ab33f47aabdaf82f
username <165684871+bikesh061@users.noreply.github.com>
2025-12-29 11:18:42 +0545

    First,

---
+++ Week1/Day3/DAy4Project.py
@@ -0,0 +1,17 @@
+
+#build a  cli calculator      command line interface
+'''
+1. take first number input from user  (int)
+
+2. take second number input from user  (int)
+
+3. take operation input from user : +,-,*,/
+
+4.1. take first number input from user
+
+Use if-else ladder to perform thoes operatrion
+
+5. if the user's operator input is invalid display appropriated message
+
+6. Note: the output printed should be in float datatype
+'''
