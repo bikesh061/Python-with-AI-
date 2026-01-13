3da805b2e24baca0fed0de5404d4d5c86f3034a1
username <165684871+bikesh061@users.noreply.github.com>
2026-01-13 12:13:21 +0545

    Update

--- Week1/Week 4 jan 12/oop.py
+++ Week1/Week 4 jan 12/oop.py
@@ -9,6 +9,12 @@ class bike:
     Wheel = 2
     Spoiler = True
 
+    #Constructor
+    def __init__(self,color,headlight,Wheel):
+        self.color = color
+        self.headlight = headlight
+        self.spoiler = spoiler
+
     # methods
     def accelerate(self):
         print('bike is accelerating')
@@ -16,8 +22,8 @@ class bike:
     def brake(self):
         print('bike is stop')
 
-# Creating ans object od the class.
-myBike = bike()
+# Creating an object od the class.
+myBike = bike('red','square','60km/h', True
 #
 # #Accesing attribute of an object
 # # print(myBike.color)
